dfc8177cf86775b974c977a83720a1f58b48bb9b
wendy <wenchingwen0913@gmail.com>
2020-01-08 17:40:12 +0800

    first

---
+++ Pratice/Controllers/HotSpotController.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Pratice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+
+namespace Pratice.Controllers
+{
+    public class HotSpotController : Controller
+    {
+        private int pageSize = 5;
+        // GET: HotSpot
+        public async Task<ActionResult> Index(int page =1)
+        {
+            int currentPage = page < 1 ? 1 : page;
+            string targetUri = "https://card.tycg.gov.tw/ashx/taolecardeventopendata.ashx";
+            HttpClient client = new HttpClient();
+            client.MaxResponseContentBufferSize = Int32.MaxValue;
+            var response = await client.GetStringAsync(targetUri);
+            var collection = JsonConvert.DeserializeObject<IEnumerable<Class1>>(response);
+            //ViewBag.Result = response;
+            return View(collection);
+        }
+    }
+}
---
+++ Pratice/Models/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Pratice.Models
+{
+    public class Class1
+    {
+
+        public object taolecardevent_id { get; set; }
+        [Display(Name = "優惠活動開始日期")]
+        public string event_start_date { get; set; }
+        [Display(Name = "優惠活動結束日期")]
+        public string event_end_date { get; set; }
+        [Display(Name = "優惠活動名稱")]
+        public string event_name { get; set; }
+        [Display(Name = "優惠活動說明")]
+        public string event_content { get; set; }
+        public object event_note { get; set; }
+        [Display(Name = "郵遞區號")]
+        public string event_address1 { get; set; }
+        [Display(Name = "縣市")]
+        public string event_address2 { get; set; }
+        [Display(Name = "鄉鎮區")]
+        public string event_address3 { get; set; }
+        [Display(Name = "地址")]
+        public string event_address4 { get; set; }
+        [Display(Name = "優惠活動電話")]
+        public string event_phone { get; set; }
+        [Display(Name = "特約商店傳真")]
+        public string store_fax { get; set; }
+        [Display(Name = "特約商店email")]
+        public string store_email { get; set; }
+        [Display(Name = "優惠活動網址")]
+        public string event_url { get; set; }
+        [Display(Name = "建立日期")]
+        public string create_date { get; set; }
+        [Display(Name = "更新日期")]
+        public string update_date { get; set; }
+        [Display(Name = "商店logo")]
+        public string strstorelogopath { get; set; }
+        [Display(Name = "優惠活動圖片")]
+        public string streventimgpath { get; set; }
+
+
+    }
+}
